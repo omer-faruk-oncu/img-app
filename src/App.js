9dfbe77d989ab4f0487829a255bdfa0dc54bfba2
Omer <farukoncu78@gmail.com>
2024-03-20 15:22:54 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+
 
 function App() {
+  const arr = data.map(item =>item.src.large)
+  console.log(arr)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">Image Gallery</div>
+      <div className="container">
+        {
+          data.map((item, index)=>(
+            <div key={index} className="item">
+              <img src={item.src.large} alt="" />
+              <p>{item.photographer}</p>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
